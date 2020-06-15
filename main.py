da90c392ee3afd1a6a801ebe7fff016e0c63d5a5
Dev Menon <dev.n.menon@gmail.com>
2020-06-15 14:14:10 -0500

    background color

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ class AlienInvasion:
             self.screen = pugame.display.set_mode(1200,800)
             pygame.display.set_caption("Alien Invasion")
 
+            # set background color
+            self.bg_color =(230,230,230)
+
+
         def run_game(self):
             """Start main game loop"""
             while True:
@@ -20,7 +24,11 @@ class AlienInvasion:
                 for event in pygame.event.get():
                         if event.type == pygame.QUIT:
                             sys.exit()
-                #make most recently drawsn scene visible
+
+                #redraw screen during each pass through the loop.
+                self.screen.fill(self.bg_color)
+
+                #make most recently drawn scene visible
                 pygame.display.flip()
 
 
@@ -28,5 +36,7 @@ class AlienInvasion:
           #make game instance, run game
         ai = AlienInvasion()
         ai.run.game()
+
+
 
 
