2ecd6035619a5406aa88c0418be11f9789385e04
Dev Menon <dev.n.menon@gmail.com>
2020-06-17 13:26:36 -0500

    add ship to main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
         """overall class to manage game assets and behavior"""
@@ -20,7 +21,7 @@ class AlienInvasion:
             )
             pygame.display.set_caption("Alien Invasion")
 
-
+            self.ship = Ship(self)
 
 
         def run_game(self):
@@ -33,6 +34,8 @@ class AlienInvasion:
 
                 #redraw screen during each pass through the loop.
                 self.screen.fill(self.settings.bg_color)
+
+                self.ship.blitme()
 
                 #make most recently drawn scene visible
                 pygame.display.flip()
