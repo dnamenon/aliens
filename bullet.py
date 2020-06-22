c4fb52001ea51438e06c7c1440fc86adc0a33914
Dev Menon <dev.n.menon@gmail.com>
2020-06-22 15:14:45 -0500

    add bullets

---
+++ bullet.py
@@ -0,0 +1,33 @@
+import pygame
+
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    # class managing bullets, it inherits from sprite. the sprite package contains commands that allows us to act
+    # on the bullets as a group. I would prefer not to inherit so i may refactor this class to have that
+    # feature without having to inherit from sprite
+
+    def __init__(self, ai_game):
+        super().__init__()
+        # gives bullet access to attributes of game screen and bullet settings
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+
+        # create bullet rect at (0,0) and then set correct initial position
+        self.rect = pygame.Rect(0,0. self.settings.bullet_speed, self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        self.y = float(self.rect.y)
+
+    def update(self):
+        # update the decimal position of the bullet
+        self.y -= self.settings.bullet_speed
+
+        #update rect
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        #draw bullet to screen
+        pygame.draw.rect(self.screen,self.color,self.rect)
+
--- settings.py
+++ settings.py
@@ -7,3 +7,9 @@ class Settings:
         self.screen_height = 600
         self.bg_color = (160, 120, 110)
         self.ship_speed = 4
+
+        #bullet settings
+        self.bullet_speed = 2.0
+        self.bullet_width = 2
+        self.bullet_height = 10
+        self.bullet_color = (0,0,0)
