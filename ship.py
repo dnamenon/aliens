0fa7e65aa75de8402f1293d68d40a05a9aea0dcf
Dev Menon <dev.n.menon@gmail.com>
2020-06-22 14:06:51 -0500

    left and right bounds

--- ship.py
+++ ship.py
@@ -48,9 +48,9 @@ class Ship:
 
     def update(self):
         # update ships position in response to arrow keys
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.sets.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.sets.ship_speed
 
         #update rect position to adjusted position of self.x
